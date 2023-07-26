6ba996463346950067aa796d2a672582e5904dc6
Moorisong <thiagoo@naver.com>
2023-07-26 15:33:25 +0900

    fix: Update latest test count from 10 to 5, change UI styels

--- src/containers/admin/index.jsx
+++ src/containers/admin/index.jsx
@@ -4,7 +4,6 @@ import { useEffect } from 'react';
 
 import { decodeToken } from '@/utils/util';
 
-import styles from './index.module.css';
 import Footer from '@/components/Footer';
 import TestAdd from '@/components/TestAdd';
 
@@ -14,12 +13,5 @@ export default function Admin() {
   useEffect(() => {
     if (decodeToken().role !== 'ROLE_ADMIN') return router.push('/main');
   }, []);
-  return (
-    <div className={styles.wrap}>
-      <TestAdd />
-      <div className={`${styles.bgWhite} ${styles.footerWrap}`}>
-        <Footer />
-      </div>
-    </div>
-  );
+  return <TestAdd />;
 }
