3008663e5699e4b31189e52e7725de22603ca6b5
Moorisong <thiagoo@naver.com>
2023-07-28 21:24:15 +0900

    fix: Request api after clicking link copy button

--- src/components/TestResult/index.jsx
+++ src/components/TestResult/index.jsx
@@ -354,6 +354,31 @@ export default function TestResult(props) {
         router.push('/login');
       });
   }
+  function clickLinkCopy() {
+    const headers = getHeaders();
+    const memeberId = sessionStorage.getItem('mongBitmemeberId') || 'anonymous';
+
+    axios
+      .post(
+        `${DOMAIN_BE_PROD}/api/v1/tests/share`,
+        {
+          testId: data.testId,
+          memberId: memeberId,
+          type: 'LINK',
+        },
+        { headers },
+      )
+      .then((res) => {
+        setCommentIndex([0, res.data.hasNextPage]);
+        setCommentChanged(!commentChanged);
+      })
+      .catch((err) => {
+        alert(err.response.data);
+        router.push('/login');
+      });
+
+    setLinkCopyState(true);
+  }
   return (
     <div className={styles.resultWrap}>
       <img className={styles.resultImg} src={props.imgUri} alt="test_result_image" />
@@ -371,12 +396,7 @@ export default function TestResult(props) {
 
       <div className={styles.buttonsWrap}>
         <div className={styles.partWrap}>
-          <div
-            className={styles.linkCopyWrap}
-            onClick={() => {
-              setLinkCopyState(true);
-            }}
-          >
+          <div className={styles.linkCopyWrap} onClick={clickLinkCopy}>
             <CopyToClipboard text={`${DOMAIN}${resultPathName}`}>
               <button className={linkCopyState ? styles.linkCopied : styles.noneLinkCopied}></button>
             </CopyToClipboard>
