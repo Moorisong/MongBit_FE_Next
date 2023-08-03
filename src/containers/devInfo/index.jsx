e69208e70023061296ac3a439e03387b4239614b
Moorisong <thiagoo@naver.com>
2023-08-04 03:32:37 +0900

    fix: Update devInfo page content
    - 내용 변경 및 Footer 제거

--- src/containers/devInfo/index.jsx
+++ src/containers/devInfo/index.jsx
@@ -1,5 +1,6 @@
+import Link from 'next/link';
+
 import styles from './index.module.css';
-import Footer from '../../components/Footer';
 
 export default function DevInfo() {
   return (
@@ -7,10 +8,25 @@ export default function DevInfo() {
       <div className={styles.textBox}>
         <ul>
           <li>
-            <p> 프론트엔드 : 김송현</p>
+            <span> GitHub &nbsp; : &nbsp; </span>
+            <span className={styles.gitHub}>
+              {' '}
+              프론트엔드
+              <Link href="https://github.com/Moorisong/MongBit_FE_Next" />
+            </span>
+            <span className={styles.gitHub}>
+              {' '}
+              백엔드
+              <Link href="https://github.com/WillNeiman/MongBit_Backend" />
+            </span>
           </li>
           <li>
-            <p>백엔드 : 임건재, 김정은, 안혜지</p>
+            <span>Instagram &nbsp; : &nbsp; </span>
+            <span className={styles.instagram}>
+              {' '}
+              몽빗 계정
+              <Link href="https://www.instagram.com/mongbit_" />
+            </span>
           </li>
         </ul>
       </div>
@@ -20,9 +36,7 @@ export default function DevInfo() {
         <p className={styles.logoText}>© 2023 MongMoongCrew. All rights reserved</p>
       </div>
 
-      <div className={`${styles.bgWhite} ${styles.footerWrap}`}>
-        <Footer />
-      </div>
+      <div className={`${styles.bgWhite} ${styles.footerWrap}`}></div>
     </div>
   );
 }
