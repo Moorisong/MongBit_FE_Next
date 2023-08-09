f0721d88f1549a75e63b8fe11b30d16ac3e3e5c3
Moorisong <thiagoo@naver.com>
2023-08-09 21:25:39 +0900

    fix: Make top content component

--- src/components/Dashboard/CountShowContent/index.jsx
+++ src/components/Dashboard/CountShowContent/index.jsx
@@ -1,6 +1,12 @@
-import { numberFormatToKoreanStyle } from '@/utils/util';
+import { getHeaders, numberFormatToKoreanStyle } from '@/utils/util';
+import { apiBe } from '@/services';
 
 import styles from './index.module.css';
+import { TitleInDashboard } from '@/components/Titles';
+import {SelectBoxDashboard} from '../SelectBox'
+import { useEffect, useState } from 'react';
+
+const topContentsSelectArr = ['Palys', 'Shares', 'Likes']
 
 export function CountCardWithColor(props) {
   const contentNumber = numberFormatToKoreanStyle(props.count);
@@ -19,5 +25,37 @@ export function CountCardWithColor(props) {
 }
 
 export function CountTopContents() {
-  return <div>hello</div>;
+  const [selectValue, setSelectValue] = useState(topContentsSelectArr[0])
+  useEffect(()=>{
+    console.log('selectValue--> ', selectValue)
+    // let requestType = selectValue === 'Plays' ? 'tests' : selectValue.toLowerCase()
+    const headers = getHeaders()
+    const queryParams = {
+      quantity: 3
+    }
+    // apiBe.get(`/api/v2/metrics/${selectValue}`, {
+    //   headers: headers,
+    //   params: queryParams,
+    // })
+    // .then((res)=>{
+    //   console.log('r-->', res.data)
+    // })
+  }, [selectValue])
+
+  function onChangeSelectValue(evt){
+    let resultValue = evt.currentTarget.value === 'Plays' ? 'tests' : evt.currentTarget.value
+    setSelectValue(resultValue)
+    //ksh 여기서 잘 들어가는지 확인, selectBox 다시 만들기
+  }
+
+  return <div className={styles.countTopContentsWrap}>
+    <div className={styles.titleArea}>
+      <TitleInDashboard text='Top Contents' />
+      <SelectBoxDashboard
+      valueArr={topContentsSelectArr}
+      name='topContent'
+      onChange = {onChangeSelectValue}/>
+    </div>
+
+  </div>;
 }
