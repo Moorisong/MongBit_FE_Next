2c4e862873188cc212e7f526303c99faace212f0
Moorisong <thiagoo@naver.com>
2023-08-09 19:04:52 +0900

    fix: Update standard meta description text
    - 테스트 프리뷰 페이지는 따로 동적으로 업데이트 예정

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -4,7 +4,7 @@ import Main from '@/containers/main';
 export async function generateMetadata() {
   const url = `${DOMAIN}/`;
   const title = '몽빗 | MBTI 심리테스트 공작소';
-  const description = 'MBTI 심리테스트 공작소';
+  const description = '조심스런 우리의 과감한 커뮤니케이션, MBTI 심리테스트 사이트 몽빗에 어서와!';
   let imageUrl = `${DOMAIN}/main/opengraph-image`;
 
   return {
--- src/app/test/latest/page.jsx
+++ src/app/test/latest/page.jsx
@@ -4,7 +4,7 @@ import TestLatest from '@/containers/testLatest';
 export async function generateMetadata({ params: { testId, testResultId } }) {
   const url = `${DOMAIN}/test/latest`;
   const title = '몽빗 | MBTI 심리테스트 공작소';
-  const description = 'MBTI 심리테스트 공작소';
+  const description = '조심스런 우리의 과감한 커뮤니케이션, MBTI 심리테스트 사이트 몽빗에 어서와!';
   let imageUrl = `${DOMAIN}/record/${testId}/${testResultId}/opengraph-image`;
 
   return {
