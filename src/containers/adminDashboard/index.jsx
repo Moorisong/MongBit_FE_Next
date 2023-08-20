2a05d337c9abc672e77f3e6f50b5dca0f52f36f6
Moorisong <thiagoo@naver.com>
2023-08-20 18:17:08 +0900

    fix: Not allow to reach admin page without 'Admin' role

--- src/containers/adminDashboard/index.jsx
+++ src/containers/adminDashboard/index.jsx
@@ -1,8 +1,9 @@
 'use client';
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 import { apiBe } from '@/services';
-import { getHeaders, formatCurrentDateTime } from '@/utils/util';
+import { getHeaders, formatCurrentDateTime, decodeToken } from '@/utils/util';
 
 import { CountCardWithColor, CountTopContents } from '@/components/Dashboard/CountShowContent';
 import { MetricsBarChartDashboard } from '@/components/Dashboard/BarChart';
@@ -13,89 +14,100 @@ const colorArr = ['#FF3F3F', '#3F80FF', '#3FDCFF', '#FF9B3F', '#FF3FD5', '#93FF3
 const countCardWithColorNames = ['방문', '플레이', '로그인', '공유', '링크복사', '좋아요', '댓글'];
 
 export default function AdminDashboard() {
+  const router = useRouter();
+  const [hasRole, setHasRole] = useState(false);
   const [totalCountCardWithColorData, setTotalCountCardWithColorData] = useState([]);
   const [todayCountCardWithColorData, setTodayCountCardWithColorData] = useState([]);
-
   const headers = getHeaders();
 
   useEffect(() => {
-    apiBe.get('/api/v2/metrics/total', { headers }).then((res) => {
-      const keyArr = Object.keys(res.data);
-      const resultArr = keyArr.map((d) => ({ totalCount: res.data[d] }));
-      setTotalCountCardWithColorData(resultArr);
-    });
+    if (decodeToken().role !== 'ROLE_ADMIN') return router.push('/');
+    setHasRole(true);
   }, []);
 
   useEffect(() => {
-    const todayTimeRange = formatCurrentDateTime();
-    const params = {
-      startDate: todayTimeRange.startDate,
-      endDate: todayTimeRange.endDate,
-    };
+    if (hasRole) {
+      apiBe.get('/api/v2/metrics/total', { headers }).then((res) => {
+        const keyArr = Object.keys(res.data);
+        const resultArr = keyArr.map((d) => ({ totalCount: res.data[d] }));
+        setTotalCountCardWithColorData(resultArr);
+      });
+    }
+  }, [hasRole]);
 
-    apiBe.get('/api/v2/metrics/total/date-range', { headers, params }).then((res) => {
-      const keyArr = Object.keys(res.data[0]);
-      const resultArr = keyArr.map((d) => ({ todayCount: res.data[0][d] }));
-      resultArr.splice(0, 1);
-      setTodayCountCardWithColorData(resultArr);
-    });
-  }, []);
+  useEffect(() => {
+    if (hasRole) {
+      const todayTimeRange = formatCurrentDateTime();
+      const params = {
+        startDate: todayTimeRange.startDate,
+        endDate: todayTimeRange.endDate,
+      };
 
-  return (
-    // 배경, 사이드 바
-    <div className={styles.wrap}>
-      <div>
-        <div className={styles.menuBar}>
-          <p>몽빗 어드민</p>
-          <ul>
-            <li>
-              <img src="/images/adminDashboard/menuBar_dashboard.svg" alt="" />
-              <span>대시보드</span>
-            </li>
-            <li>
-              <img src="/images/adminDashboard/menuBar_contents.svg" alt="" />
-              <span>컨텐츠</span>
-            </li>
-            <li>
-              <img src="/images/adminDashboard/menuBar_members.svg" alt="" />
+      apiBe.get('/api/v2/metrics/total/date-range', { headers, params }).then((res) => {
+        const keyArr = Object.keys(res.data[0]);
+        const resultArr = keyArr.map((d) => ({ todayCount: res.data[0][d] }));
+        resultArr.splice(0, 1);
+        setTodayCountCardWithColorData(resultArr);
+      });
+    }
+  }, [hasRole]);
 
-              <span>유저</span>
-            </li>
-            <li>
-              <img src="/images/adminDashboard/menuBar_insight.svg" alt="" />
+  if (hasRole)
+    return (
+      // 배경, 사이드 바
+      <div className={styles.wrap}>
+        <div>
+          <div className={styles.menuBar}>
+            <p>몽빗 어드민</p>
+            <ul>
+              <li>
+                <img src="/images/adminDashboard/menuBar_dashboard.svg" alt="" />
+                <span>대시보드</span>
+              </li>
+              <li>
+                <img src="/images/adminDashboard/menuBar_contents.svg" alt="" />
+                <span>컨텐츠</span>
+              </li>
+              <li>
+                <img src="/images/adminDashboard/menuBar_members.svg" alt="" />
 
-              <span>인사이트</span>
-            </li>
-          </ul>
-        </div>
+                <span>유저</span>
+              </li>
+              <li>
+                <img src="/images/adminDashboard/menuBar_insight.svg" alt="" />
 
-        {/* 콘텐츠 내용 */}
-        <div className={styles.contentWrap}>
-          <div>
-            <TitleInDashboard text="대시보드" />
+                <span>인사이트</span>
+              </li>
+            </ul>
           </div>
 
-          <div>
-            <div className={styles.countCardWithColorArea}>
-              {totalCountCardWithColorData &&
-                totalCountCardWithColorData.map((d, i) => (
-                  <CountCardWithColor
-                    key={i}
-                    title={countCardWithColorNames[i]}
-                    totalCount={d.totalCount}
-                    todayCount={todayCountCardWithColorData[i].todayCount}
-                    borderColor={colorArr[i]}
-                  />
-                ))}
+          {/* 콘텐츠 내용 */}
+          <div className={styles.contentWrap}>
+            <div>
+              <TitleInDashboard text="대시보드" />
             </div>
 
-            <div className={styles.flexDirRow}>
-              <MetricsBarChartDashboard />
-              <CountTopContents />
+            <div>
+              <div className={styles.countCardWithColorArea}>
+                {totalCountCardWithColorData &&
+                  totalCountCardWithColorData.map((d, i) => (
+                    <CountCardWithColor
+                      key={i}
+                      title={countCardWithColorNames[i]}
+                      totalCount={d.totalCount}
+                      todayCount={todayCountCardWithColorData[i].todayCount}
+                      borderColor={colorArr[i]}
+                    />
+                  ))}
+              </div>
+
+              <div className={styles.flexDirRow}>
+                <MetricsBarChartDashboard />
+                <CountTopContents />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
